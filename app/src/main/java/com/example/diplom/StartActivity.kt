718a05773fe9579768b1ee95206b14672965b887
morfi <Morfij20@gmail.com>
2023-10-20 20:15:09 +0300

    ур16

---
+++ app/src/main/java/com/example/diplom/StartActivity.kt
@@ -0,0 +1,20 @@
+package com.example.diplom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.diplom.databinding.ContentStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ContentStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ContentStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
